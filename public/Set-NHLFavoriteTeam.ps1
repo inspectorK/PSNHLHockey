40b73662cf5926ad3657c6e08bad784e87368d1d
afink-osi <afink@osisoft.com>
2019-02-21 12:00:21 -0500

    Set-NHLFavoriteTeam now supports saving across sessions

--- public/Set-NHLFavoriteTeam.ps1
+++ public/Set-NHLFavoriteTeam.ps1
@@ -1,6 +1,6 @@
 <#region HELP
 .SYNOPSIS 
-Set the user's favorite NHL team to persist between sessions
+Set the user's favorite NHL team
 
 .DESCRIPTION
 This cmdlet allows the user to set their favorite NHL team.  This team will be used in supported cmdlets
@@ -9,10 +9,18 @@ by default if a TeamName or TeamID parameter are ommitted.
 .PARAMETER TeamName
 The NHL team name to set as favorite.  The team name is an NHLTeam enum.
 
+.PARAMETER Save
+Use this parameter to save your favorite team and have it persist throughout sessions
+
 .EXAMPLE
 Set-NHLFavoriteTeam -TeamName PhiladelphiaFlyers
 
 This example will set the user's favorite NHL Team to be the Philadelphia Flyers.
+
+.EXAMPLE
+Set-NHLFavoriteTeam -TeamName PhiladelphiaFlyers -Save
+
+This example will set the user's favorite NHL Team to be the Philadelphia Flyers.  This setting will be saved and persist across sessions
 
 #>
 
@@ -22,24 +30,31 @@ function Set-NHLFavoriteTeam {
     # Parameter help description
     [Parameter(Mandatory=$true, Position=0)]
     [NHLTeams]
-    $TeamName
+    $TeamName,
+    [Parameter()]
+    [switch]
+    $Save
     )
 
     process {      
         # Generate SavePath
         [string]$SavePath = (Join-Path (Split-Path -Path $PSScriptRoot -Parent) "settings.xml")
 
-        # update PSNHL_SETTINGS in memory
-        $Global:PSNHL_Settings.settings.data.favoriteTeamName = $TeamName.ToString
-        $Global:PSNHL_Settings.settings.data.favoriteTeamID = (Get-NHLTeamID -TeamName $TeamName)
+        # update PSNHL_SETTINGS in memory for just this session
+        $Global:PSNHL_Settings.settings.data.favoriteTeamName = $TeamName.ToString()
+        $Global:PSNHL_Settings.settings.data.favoriteTeamID = (Get-NHLTeamID -TeamName $TeamName).ToString()
 
-        try 
+        # user wants to persist settings
+        if ($Save)
         {
-            $Global:PSNHL_Settings.Save($SavePath)
-        }
-        catch
-        {
-            Write-Error "Error saving favorite teams settings to disk at path: $SavePath Your favorite team settings will still persist for this session."
+            try 
+            {
+                $Global:PSNHL_Settings.Save($SavePath)
+            }
+            catch
+            {
+                Write-Error "Error saving favorite teams settings to disk at path: $SavePath Your favorite team settings will still persist for this session."
+            }
         }
     }
 }
